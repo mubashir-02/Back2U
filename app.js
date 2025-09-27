e812d6c247289dc252c3089f7d19a3d2a9d12436
mubashir-02 <nn5077@srmist.edu.in>
2025-09-27 13:38:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -147,6 +147,7 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on port ${PORT}`);
 });
