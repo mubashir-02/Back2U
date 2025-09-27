2c33ae5359d91764c96e48d1e65bb62d9ebb4f6c
mubashir-02 <nn5077@srmist.edu.in>
2025-09-27 10:23:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -145,6 +145,6 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(8080,()=>{
-    console.log("Server is running on port 8080");
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
 });
