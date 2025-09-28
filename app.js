99319e44c1c43349eda9d4692c5d829b8b88a544
mubashir-02 <nn5077@srmist.edu.in>
2025-09-28 10:03:50 +0530

    Describe your changes here

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(` Server is running on port ${PORT}`);
 });
